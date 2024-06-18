e811434fe85e8522f2c0057813c42af9adafd294
Salimov Sergei <sergesalimov@yandex.ru>
2024-06-18 23:01:28 +0300

    Update server code with docker support

--- srv.dart
+++ srv.dart
@@ -1,106 +1,140 @@
+import 'dart:async';
 import 'dart:io';
-import 'package:mysql1/mysql1.dart';
+
+import 'package:mysql_client/mysql_client.dart';
+
+var hostDockerInternalAddress;
+final collation = "utf8mb4_unicode_ci";
 
 void main() async {
-	// запуск web-сервера.
-  var server = await HttpServer.bind(InternetAddress.anyIPv6, 8888);
-  print("Listening for connections on http://localhost:8888/");
+  var server = await HttpServer.bind(InternetAddress.anyIPv4, 8888);
+  print("Listening for connections on http://host.docker.internal:8888/");
+  hostDockerInternalAddress = await getHostDockerInternalAddress();
+
   await server.forEach((HttpRequest request) {
- 
     switch (request.uri.path) {
-		case "/":
-			//print(request.uri);
-			var uri = Uri.parse(request.uri.toString());
-			uri.queryParameters.forEach((k, v) {
-				print('key: $k - value: $v');
-			});
-			print('Count: '+uri.queryParameters.length.toString());
-			if (uri.queryParameters.length > 0) {
-				// добавление одной строки в таблицу.
-				rowInsert(uri.queryParameters);
-			}
-			break;
+      case "/":
+        print(request.uri);
+        var uri = Uri.parse(request.uri.toString());
+        uri.queryParameters.forEach((k, v) {
+          print('key: $k - value: $v');
+        });
+        print('Count: ' + uri.queryParameters.length.toString());
+        if (uri.queryParameters.length > 0) {
+          rowInsert(uri.queryParameters);
+        }
+        break;
     }
     final res = request.response;
-	ReadTpl(res);
-
+    ReadTpl(res);
   });
 }
 
+Future<String> getHostDockerInternalAddress() async {
+  final addresses = await InternetAddress.lookup('host.docker.internal');
+  return addresses.isNotEmpty ? addresses.first.address : '192.168.0.102';
+}
+
 void ReadTpl(res) async {
-	res.headers.add(HttpHeaders.contentTypeHeader, "text/html; charset=utf-8");
-	File file = File("select.html");
-	var lines = await file.readAsLines();
-	for(final line in lines){
-	//	print(line);
-		
-		if ((line != "@tr") && (line != "@ver")) {
-			res.write(line);
-		}
-		if (line.contains("@tr")) {
-			String message = await viewSelect(res);
-		}
-		if (line.contains("@ver")) {
-			String message = await viewVer(res);
-		}		
-	}
-	 
-	res.close();
-	print("User is ok.");
+  res.headers.add(HttpHeaders.contentTypeHeader, "text/html; charset=utf-8");
+  File file = File("select.html");
+  var lines = await file.readAsLines();
+  for (final line in lines) {
+    if (line.contains("@tr")) {
+      await viewSelect(res);
+    } else if (line.contains("@ver")) {
+      await viewVer(res);
+    } else {
+      res.write(line);
+    }
+  }
+  res.close();
 }
 
-Future<String> viewSelect(res) async {
-	final conn = await MySqlConnection.connect(new ConnectionSettings(host: '127.0.0.1',port: 3306,user: 'root',/*password: '',*/db: 'test',));
-	res.write('<table>');
-	var heads = await conn.query("SHOW COLUMNS FROM myarttable");
-	res.write('<tr>');
-	for (var head in heads) {
-		res.write('<td>${head[0]}</td>');
-	//	print('${head[0]}');
-	}
-	res.write('</tr>');	
-	
-	var rows = await conn.query("SELECT * FROM myarttable WHERE id>14 ORDER BY id DESC");
-	for (var row in rows) {
-		res.write('<tr>');
-		for (var col in row) {
-			res.write('<td>${col}</td>');
-		//	print('${col}');
-		}
-		res.write('</tr>');
-	}
-	res.write('</table>');
-	await conn.close();
-	return Future.delayed(Duration(seconds: 0), () => "Hello Dart");
+Future<void> viewSelect(res) async {
+  try {
+    final conn = await MySQLConnection.createConnection(
+      host: hostDockerInternalAddress,
+      port: 3306,
+      userName: "root",
+      password: "qwerty",
+      databaseName: "test",
+      collation: collation,
+    );
+    await conn.connect();
+    var heads = await conn.execute("SHOW COLUMNS FROM Individuals");
+    res.write('<tr>');
+    for (var head in heads.rows) {
+      res.write('<td>${head.colAt(0)}</td>');
+    }
+    res.write('</tr>');
+
+    var table = await conn.execute(
+        "SELECT * FROM Individuals ORDER BY last_name, first_name, middle_name");
+    final numOfCols = table.numOfColumns;
+    for (var row in table.rows) {
+      res.write('<tr>');
+      for (var i = 0; i < numOfCols; i++) {
+        var val = row.colAt(i);
+        res.write('<td>${val}</td>');
+      }
+      res.write('</tr>');
+    }
+    await conn.close();
+  } on Exception catch (e) {
+    res.write('WAITING FOR SQL SERVER TO SETUP');
+    print('viewSelect: $e');
+  }
 }
 
-Future<String> viewVer(res) async {
-	final conn = await MySqlConnection.connect(new ConnectionSettings(host: '127.0.0.1',port: 3306,user: 'root',/*password: '',*/db: 'test',));
-	var vers = await conn.query("SELECT VERSION() AS ver");
-	for (var ver in vers) {
-		res.write('${ver[0]}');
-		print('${ver[0]}');
-	}
-	await conn.close();
-	return Future.delayed(Duration(seconds: 0), () => "Hello Dart");
+Future<void> viewVer(res) async {
+  try {
+    final conn = await MySQLConnection.createConnection(
+        host: hostDockerInternalAddress,
+        port: 3306,
+        userName: "root",
+        password: "12345678",
+        databaseName: "lab_1",
+        collation: collation);
+    await conn.connect();
+    var vers = await conn.execute("SELECT VERSION() AS ver");
+    for (var ver in vers.rows) {
+      res.write('${ver.colAt(0)}');
+    }
+    await conn.close();
+  } on Exception catch (e) {
+    res.write('WAITING FOR SQL SERVER TO SETUP');
+    print('viewVer: $e');
+  }
 }
 
-Future<String> rowInsert(mass) async {
+Future<void> rowInsert(mass) async {
+  try {
+    print('rowInsert');
+    String sValue = '';
+    int i = 0;
+    mass.forEach((k, v) {
+      if (i > 0) {
+        sValue = sValue + ',';
+      }
+      sValue = sValue + "'$v'";
+      i++;
+    });
+    sValue =
+        'INSERT INTO Individuals (last_name, first_name, middle_name, passport, tax_number, social_number, driver_license, documents, notes) VALUES ($sValue)';
 
-	String sValue = '';
-	int i=0;
-	mass.forEach((k, v) {
-		//print('key: $k - value: $v');
-		if (i>0){sValue = sValue+',';}
-		sValue = sValue+"'$v'";
-		i++;
-	});
-	sValue = 'INSERT INTO myarttable (text, description, keywords) VALUES ('+sValue+')';
-	
-	final conn = await MySqlConnection.connect(new ConnectionSettings(host: '127.0.0.1',port: 3306,user: 'root',/*password: '',*/db: 'test',));
-	await conn.query(sValue);
-	await conn.close();
-	print('Insert into table is good.');
-
-	return Future.delayed(Duration(seconds: 0), () => "Hello Dart");
+    final conn = await MySQLConnection.createConnection(
+        host: hostDockerInternalAddress,
+        port: 3306,
+        userName: "root",
+        password: "12345678",
+        databaseName: "lab_1",
+        collation: collation);
+    await conn.connect();
+    await conn.execute(sValue);
+    await conn.close();
+    print('Insert into table is good.');
+  } on Exception catch (e) {
+    print('rowInsert: $e');
+  }
 }
